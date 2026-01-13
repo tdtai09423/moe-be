0adf9a34b763adde9c1f34844a85347e7d36ad26
c-kain.tran <c-kain.tran@avepoint.com>
2026-01-13 16:37:34 +0700

    change config of swagger in eservice portal

--- EServicePortal/src/MOE_System.EService.API/ServicesRegister/SwaggerServiceRegistration.cs
+++ EServicePortal/src/MOE_System.EService.API/ServicesRegister/SwaggerServiceRegistration.cs
@@ -12,17 +12,14 @@ public static class SwaggerServiceRegistration
 
     public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MOE EService API v1");
-                c.RoutePrefix = "swagger";
-                c.DocumentTitle = "MOE EService API Documentation";
-                c.DisplayRequestDuration();
-            });
-        }
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "MOE System API v1");
+            c.RoutePrefix = "swagger";
+            c.DocumentTitle = "MOE System API Documentation";
+            c.DisplayRequestDuration();
+        });
 
         return app;
     }
